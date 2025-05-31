8ad9902f36f0776d0f5be04f89d485fb9f34708d
tsuba-28 <ttsubasa2041@gmail.com>
2025-05-31 20:29:10 +0900

    Aboutページ、HOMEページへのリンクを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
   get '/' => 'home#index'
   get 'about' => 'home#about'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
