612970439ca1ad3ce89ddf54dcdfb5bb70084294
Tsuba <>
2025-06-18 21:27:21 +0900

    自分の記事に対してのみアクションボタンを表示するように変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,4 +23,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :articles, dependent: :destroy
+
+  def has_written?(article)
+    article.exists?(id: article.id)
+  end
 end
