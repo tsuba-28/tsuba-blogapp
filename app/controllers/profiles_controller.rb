daa744632922b5477ae6a1b53cbe9a5160a317dc
Tsuba <>
2025-06-21 22:12:45 +0900

    プロフィールを更新できるように変更

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,16 +6,17 @@ class ProfilesController < ApplicationController
   end
 
   def edit
-    @profile = current_user.build_profile
+    @profile = current_user.prepare_profile
   end
 
   def update
-    @profile = current_user.build_profile(profile_params)
+    @profile = current_user.prepare_profile
+    @profile.assign_attributes(profile_params)
     if @profile.save
       redirect_to profile_path, notice: 'プロフィール更新'
     else
       flash.now[:error] = '更新できませんでした'
-      render :edit#, status: :unprocessable_entity
+      render :edit
     end
   end
 
