bfca14048fc41fae661131824f1b5baff35609c5
Tsuba <>
2025-06-23 23:38:15 +0900

    スペース幅を２スペースに変更して統一

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,56 +1,56 @@
 class ArticlesController < ApplicationController
-    before_action :set_article, only: [:show]
-    before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_article, only: [:show]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
-    def index
-        @articles = Article.all
-    end
+  def index
+    @articles = Article.all
+  end
 
-    def show
-        @comments = @article.comments
-    end
+  def show
+    @comments = @article.comments
+  end
 
-    def new
-        @article = current_user.articles.build
-    end
+  def new
+    @article = current_user.articles.build
+  end
 
-    def create
-        @article = current_user.articles.build(article_params)
-        if @article.save
-            redirect_to article_path(@article), notice: '保存できました！'
-        else
-            flash.now[:error] = '保存に失敗しました'
-            render :new, status: :unprocessable_entity
-        end
+  def create
+    @article = current_user.articles.build(article_params)
+    if @article.save
+      redirect_to article_path(@article), notice: '保存できました！'
+    else
+      flash.now[:error] = '保存に失敗しました'
+      render :new, status: :unprocessable_entity
     end
+  end
 
-    def edit
-        @article = current_user.articles.find(params[:id])
-    end
+  def edit
+    @article = current_user.articles.find(params[:id])
+  end
 
-    def update
-        @article = current_user.articles.find(params[:id])
-        if @article.update(article_params)
-            redirect_to article_path(@article), notice: '更新できました!'
-        else
-            flash.now[:error] = '更新できませんでした'
-            render :edit, status: :unprocessable_entity
-        end
+  def update
+    @article = current_user.articles.find(params[:id])
+    if @article.update(article_params)
+      redirect_to article_path(@article), notice: '更新できました!'
+    else
+      flash.now[:error] = '更新できませんでした'
+      render :edit, status: :unprocessable_entity
     end
+  end
 
-    def destroy
-        article = current_user.articles.find(params[:id])
-        article.destroy!
-        redirect_to root_path, status: :see_other, notice: '削除に成功しました!'
-    end
+  def destroy
+    article = current_user.articles.find(params[:id])
+    article.destroy!
+    redirect_to root_path, status: :see_other, notice: '削除に成功しました!'
+  end
 
-    private
-    def article_params
-        params.require(:article).permit(:title, :content)
-    end
+  private
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
 
-    def set_article
-        @article = Article.find(params[:id])
-    end
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -14,30 +14,30 @@
 #  index_articles_on_user_id  (user_id)
 #
 class Article < ApplicationRecord
-    validates :title, presence: true
-    validates :title, length: { minimum: 2, maximum: 100 }
-    validates :title, format: { with: /\A(?!\@)/ }
+  validates :title, presence: true
+  validates :title, length: { minimum: 2, maximum: 100 }
+  validates :title, format: { with: /\A(?!\@)/ }
 
-    validates :content, presence: true
-    validates :content, length: { minimum: 10 }
-    validates :content, uniqueness: true
+  validates :content, presence: true
+  validates :content, length: { minimum: 10 }
+  validates :content, uniqueness: true
 
-    validate :validate_title_and_content_length
+  validate :validate_title_and_content_length
 
-    has_many :comments, dependent: :destroy
-    belongs_to :user
+  has_many :comments, dependent: :destroy
+  belongs_to :user
 
-    def display_created_at
-        I18n.l(self.created_at, format: :default)
-    end
+  def display_created_at
+    I18n.l(self.created_at, format: :default)
+  end
 
-    def author_name
-        user.display_name
-    end
+  def author_name
+    user.display_name
+  end
 
-    private
-    def validate_title_and_content_length
-        char_count = self.title.length + self.content.length
-        errors.add(:content, '100文字以上で記述してください') unless char_count > 100
-    end
+  private
+  def validate_title_and_content_length
+    char_count = self.title.length + self.content.length
+    errors.add(:content, '100文字以上で記述してください') unless char_count > 100
+  end
 end
