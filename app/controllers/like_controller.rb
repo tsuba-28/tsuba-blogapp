8c121b36d2d7282773e464620960ab29e8c4fbb1
Tsuba <>
2025-06-24 22:42:28 +0900

    細かなバグを修正

--- app/controllers/like_controller.rb
+++
@@ -1,9 +0,0 @@
-class LikesController < ApplicationController
-  before_action :authenticate_user!
-
-  def create
-    article = Article.find(params[:article_id])
-    article.likes.create!(user_id: current_user.id)
-    redirect_to article_path(article)
-  end
-end
