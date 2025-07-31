43e87511301ec5e9f5f6482de352403b0f2d1fa0
Tsuba <>
2025-07-31 11:43:00 +0900

    like status の API を作成

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -10,10 +10,10 @@ window.$ = jquery
 import axios from 'axios'
 
 document.addEventListener('turbo:load', () => {
-  $('.article_title').on('click', () => {
-    axios.get('/')
-      .then((response) => {
-        console.log(response)
-      })
-  })
+  const dataset = $('#article-show').data()
+  const articleId = dataset.articleId
+  axios.get(`/articles/${articleId}/like`)
+    .then((response) => {
+      console.log(response)
+    })
 })
