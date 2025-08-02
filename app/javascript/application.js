41b38227a9aaba4801ed65ac27361a2d017c07cb
Tsuba <>
2025-08-02 12:36:29 +0900

    いいねの状態で表示を切り分ける

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -14,6 +14,11 @@ document.addEventListener('turbo:load', () => {
   const articleId = dataset.articleId
   axios.get(`/articles/${articleId}/like`)
     .then((response) => {
-      console.log(response)
+      const hasLiked = response.data.hasLiked
+      if (hasLiked) {
+        $('.active-heart').removeClass('hidden')
+      } else {
+        $('.inactive-heart').removeClass('hidden')
+      }
     })
 })
