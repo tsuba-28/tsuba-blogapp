79ff6e48fbf38c3ab4d29999baee6088d1a08c92
Tsuba <>
2025-08-15 22:44:52 +0900

    タブの問題、タイポを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,9 @@ class ApplicationController < ActionController::Base
   def default_url_options
     { locale: I18n.locale }
   end
+
   private
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_local
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
