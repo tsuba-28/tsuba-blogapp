fe57196540bc416e387ce7b60e87b13716ba8e7a
Tsuba <>
2025-08-15 22:29:05 +0900

    多言語化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   def current_user
     ActiveDecorator::Decorator.instance.decorate(super) if super.present?
     super
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+  private
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_local
   end
 end
